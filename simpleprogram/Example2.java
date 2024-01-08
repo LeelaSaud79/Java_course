2d1279397111348bda032024b6c1a22e0b0b379f
Leela Saud <leelasaud79@gmail.com>
2024-01-08 21:16:11 +0545

    feat:basic

---
+++ simpleprogram/Example2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Example2 {
+public static void main(String[] args)
+{
+String name,address;
+System.out.print("enter your name and address :");
+Scanner sc = new Scanner(System.in);
+name = sc.nextLine();
+address=sc.next();
+System.out.print("name="+name+"\naddress="+address);
+}
+}
