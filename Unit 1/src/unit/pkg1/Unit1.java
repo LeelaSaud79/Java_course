ef6e7d9e721be93f70360db61ef6abc5b69b7c2c
Leela Saud <leelasaud79@gmail.com>
2024-02-22 20:39:33 +0545

    unit 1

--- Unit 1/src/unit/pkg1/Unit1.java
+++ Unit 1/src/unit/pkg1/Unit1.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package unit.pkg1;
+import java.util.Scanner;
 
-/**
- *
- * @author dell
- */
 public class Unit1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the limit: ");
+        int limit = scanner.nextInt();
+        
+        System.out.println("Natural numbers up to " + limit + ":");
+        for (int i = 1; i <= limit; i++) {
+            System.out.println(i);
+        }
+        scanner.close();
     }
-    
 }
